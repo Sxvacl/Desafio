3d8b2c3b9bee88f0bcd84579d3daed8d9e44e1ab
Sxvacl <jose.diazad@mail.udp.cl>
2024-03-23 12:20:58 -0300

    feat: add question2 resolutions with two  aproach

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,18 @@
+from collections import Counter
+import pandas as pd
 from typing import List, Tuple
+from helpers.emojis import extract_emojis
 
-def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+
+def q2_memory(file_path: str, chunksize: int = 5000) -> List[Tuple[str, int]]:
+    # Inicializar el contador de emojis
+    emoji_counter = Counter()
+    
+    # Procesar el archivo JSON en fragmentos
+    for chunk in pd.read_json(file_path, lines=True, chunksize=chunksize):
+        for content in chunk['content']:
+            emojis = extract_emojis(content)
+            emoji_counter.update(emojis)
+    
+    # Devolver el top 10
+    return emoji_counter.most_common(10)
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,29 @@
+import re
+from collections import Counter
+import pandas as pd
 from typing import List, Tuple
+from helpers.emojis import extract_emojis
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """
+    Reads a JSON file line by line into a pandas DataFrame, extracts emojis from the 'content' field of each row,
+    counts the occurrences of each emoji, and returns the 10 most common emojis with their counts.
+    
+    Args:
+        file_path (str): The file path to the JSON file to be processed.
+    
+    Returns:
+        List[Tuple[str, int]]: A list of tuples where each tuple contains an emoji and its count, 
+        representing the 10 most common emojis found in the file.
+    """
+
+    df = pd.read_json(file_path, lines=True)
+    
+    emoji_counter = Counter()
+
+    for content in df['content']:
+        emojis = extract_emojis(content)
+        emoji_counter.update(emojis)
+    
+    return emoji_counter.most_common(10)
